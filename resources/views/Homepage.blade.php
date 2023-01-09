ca4d4aac910930463352bfc9cf6cc03af2ad26c1
Prathibansekar <vinothskumar4@gmail.com>
2023-01-09 12:47:21 +0530

    title changes

--- resources/views/Homepage.blade.php
+++ resources/views/Homepage.blade.php
@@ -5,10 +5,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>form tag</title>
+    <title>login page</title>
     <style>
         #style {
-            background-color: aqua;
+            background-color: chartreuse;
         }
     </style>
 </head>
@@ -17,22 +17,22 @@
     <center>
         <form method="POST" action="/send" id="style">
 
-            <label>USERNAME:<input type="text" name="username" required></label>
+            <label>USERNAME <input type="text" name="username" required></label>
             <br>
             <br>
-            <label>PASSWORD:<input type="password" name="password" required></label>
+            <label>PASSWORD <input type="password" name="password" required></label>
             <br>
             <br>
-            <label>D-O-B:<input type="date" name="dob" required></label>
+            <label>D-O-B <input type="date" name="dob" required></label>
             <br>
             <br>
             <input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
-            <label>PHONE NO:<input type="number" name="number" required></label><br>
+            <label>PHONE NO <input type="number" name="number" required></label><br>
             <br>
 
 
             <br>
-            <input type="submit" value="login" style="background-color:brown" >
+            <input type="submit" value="login" style="background-color:brown">
 
 
         </form>
